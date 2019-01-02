64672300c533a663bccc1291ed82d71640a5fe6d
Sergey D <s.dureiko@gmail.com>
2019-01-02 17:05:57 +0300

    Additional clean up work

--- Send reminder/Send approval email if it is requested.js
+++ Send reminder/Send approval email if it is requested.js
@@ -1,7 +1,9 @@
 (function() {
+	// Check if current approval request is still waiting for approval action. Exit if it is answered.
 	if(current.state != 'requested') {
 		return;
 	}
-	
+
+	// Trigger email: use event and OOB notification
 	gs.eventQueue("sc_req_item.approval.inserted", current);
 })();
--- Send reminder/Send reminder email in 4 days.js
+++ Send reminder/Send reminder email in 4 days.js
@@ -2,9 +2,8 @@
 	var util = new ScheduledEventGenerator();
 	util.setDocument(current);
 	util.setLabel("Resend approval request");
-	util.setAsSeconds(gs.getProperty("com.approvals.reminder", 60*60*24*4));
-	util.setEvent("resend_approval");
+	util.setAsSeconds(gs.getProperty("com.approvals.reminder_interval", 60*60*24*4));
+	util.setEvent("com.resend_approval");
 	util.setupScript();
 	util.schedule();
-	
 })();
